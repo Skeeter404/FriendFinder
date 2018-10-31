c309bbae44743c64a9a0cf289876638c2d1636a8
Skeeter404 <bronson.bird404@gmail.com>
2018-10-30 21:02:59 -0600

    almost final

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friendArray = [
+    {name: "Ron Swanson",
+photo: "https://jasonaguirredotcom.files.wordpress.com/2012/07/ron-swanson-rules1.jpg",
+scores: [
+  2,
+  4,
+  2,
+  5,
+  4,
+  1,
+  3,
+  5,
+  4,
+  2
+]}
+];
